eb80bd1131da3433a49593641ff7b5ee8565332c
Daniel Beeke <mail@danielbeeke.nl>
2022-04-20 13:03:41 +0200

    just js andcss

--- presentation.js
+++ presentation.js
@@ -1,7 +1,7 @@
-import Reveal from 'reveal.js';
-import Highlight from 'reveal.js/plugin/highlight/highlight.esm.js';
-import Notes from 'reveal.js/plugin/notes/notes.esm.js';
-import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js';
+import Reveal from 'https://cdn.skypack.dev/reveal.js';
+import Highlight from 'https://cdn.skypack.dev/reveal.js/plugin/highlight/highlight.esm.js';
+import Notes from 'https://cdn.skypack.dev/reveal.js/plugin/notes/notes.esm.js';
+import Markdown from 'https://cdn.skypack.dev/reveal.js/plugin/markdown/markdown.esm.js';
 
 let deck = new Reveal({
    plugins: [ Markdown, Highlight, Notes ],
